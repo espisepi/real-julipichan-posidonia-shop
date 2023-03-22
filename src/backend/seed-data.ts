4871d1f8f1be407be1dc02a77e452f4c34995d98
espisepi <sepinaco@gmail.com>
2023-03-22 12:47:39 +0100

    fixed products with different id when recharge page

--- src/backend/seed-data.ts
+++ src/backend/seed-data.ts
@@ -36,7 +36,7 @@ export const initialData: SeedData = {
             inStock: 1,
             images: ['img/waternormals.jpeg','img/waternormals.jpeg'],
             scene: 'urlscene',
-            id: uid(),
+            id: '51f5gg9siff08t4cn0jivpu', //uid() no lo usamos porque si no hay incongruencias cuando se recarga la pagina y los productos tienen distintos id de los guardado en las cookies (carrito de compra)
             createdAt: Date.now(),
             updatedAt: Date.now(),
         },
