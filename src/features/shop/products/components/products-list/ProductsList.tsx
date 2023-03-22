31ac00738e2174eabf77cce89ca1b84890d28052
espisepi <sepinaco@gmail.com>
2023-03-22 12:40:28 +0100

    logic get products added into ProductList component

--- src/features/shop/products/components/products-list/ProductsList.tsx
+++ src/features/shop/products/components/products-list/ProductsList.tsx
@@ -1,40 +1,53 @@
-import { ICartProduct } from "@/features/shop/cart/types";
-import { ProductResponse } from "../../types"
-import { productResponseToCartProductConverter } from "@/converters/productResponseToCartProductConverter";
-import { useContext } from "react";
-import { CartContext } from "@/features/shop/cart";
-
+import { ICartProduct } from '@/features/shop/cart/types'
+import { ProductResponse } from '../../types'
+import { productResponseToCartProductConverter } from '@/converters/productResponseToCartProductConverter'
+import { useContext, useEffect } from 'react'
+import { CartContext } from '@/features/shop/cart'
+import { useProducts } from '../../api'
 
 export type ProductsListProps = {
-    products: ProductResponse[];
-    isLoading: boolean;
-    tag: string;
+  tag: string
 }
 
-export const ProductsList = ({
-    products,
+export const ProductsList = ({ tag }: ProductsListProps) => {
+  
+  const {
+    data: products,
     isLoading,
-    tag
-  }: ProductsListProps ) => {
+    refetch,
+  } = useProducts({
+    params: {
+      tag: tag,
+    },
+  })
 
-    const { addProductToCart } = useContext(CartContext);
+  useEffect(() => {
+    if (products) {
+      // Ejecutamos la peticion al api endpoint con react-query
+      refetch()
+    }
+  }, [])
 
-    if(isLoading) return <h1>IS LOADING</h1>;
+  const { addProductToCart } = useContext(CartContext)
 
-    return (
-      <div id='ProductsList-component'>
-        <h2>Tag: {tag ? tag : 'Ninguna etiqueta seleccionada'}</h2>
-        {products && products?.length != 0
-          ? products.map((product, i) => (
-              <div id={`productCard-component-${i}`} key={`productCard-component-${i}`}>
-                <h2>{product.title}</h2>
-                <h2>{product.description}</h2>
-                <h2>Price: {product.price}</h2>
-                <button onClick={(e)=>addProductToCart(productResponseToCartProductConverter({product}))}> Agregar al carrito </button>
-              </div>
-            ))
-          : null}
-      </div>
-    )
+  if (isLoading) return <h1>IS LOADING</h1>
 
+  return (
+    <div id='ProductsList-component'>
+      <h2>Tag: {tag ? tag : 'Ninguna etiqueta seleccionada'}</h2>
+      {products && products?.length != 0
+        ? products.map((product, i) => (
+            <div id={`productCard-component-${i}`} key={`productCard-component-${i}`}>
+              <h2>{product.title}</h2>
+              <h2>{product.description}</h2>
+              <h2>Price: {product.price}</h2>
+              <button onClick={(e) => addProductToCart(productResponseToCartProductConverter({ product }))}>
+                {' '}
+                Agregar al carrito{' '}
+              </button>
+            </div>
+          ))
+        : null}
+    </div>
+  )
 }
--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -8,25 +8,14 @@ export default function ShopPage() {
 
     const tag = undefined;
 
-    const products = useProducts({
-        params: {
-            tag: tag,
-        },
-    });
 
-    useEffect(()=>{
-        if(products) {
-            // Ejecutamos la peticion al api endpoint con react-query
-            products.refetch();
-        }
-    },[]);
 
     // const { addProductToCart } = useCartStore;
 
     return (
       <div id='shop-page'>
         <h1>SHOP PAGE</h1>
-        <ProductsList products={products.data} isLoading={products.isLoading} tag={tag} />
+        <ProductsList tag={tag} />
         <CartProductList />
       </div>
     )
