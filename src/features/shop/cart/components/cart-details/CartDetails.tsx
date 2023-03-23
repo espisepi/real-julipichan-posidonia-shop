04927f7fd39ec71549f91930a116e17800b1128d
espisepi <sepinaco@gmail.com>
2023-03-23 12:15:03 +0100

    cartDetails with css styles

--- src/features/shop/cart/components/cart-details/CartDetails.tsx
+++ src/features/shop/cart/components/cart-details/CartDetails.tsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { CartContext } from "../../context"
 
 import { TAX_RATE_PERCENTAGE } from "@/constants";
-
+import styles from './CartDetails.module.css';
 
 
 export const CartDetails = () => {
@@ -10,27 +10,27 @@ export const CartDetails = () => {
     const { numberOfItems, subTotal, tax, total } = useContext(CartContext);
 
     return (
-      <div id='cart-details-component'>
-        <div id='cart-details-title'>
+      <div id='cart-details-component' className={styles.cart_details_component}>
+        <div id='cart-details-title' className={styles.cart_details_title}>
           <p>Detalles del Carrito</p>
         </div>
-        <div id='cart-details-number-of-products'>
+        <div id='cart-details-number-of-products' className={styles.cart_details_row}>
           <p> Numero de productos </p>
           <p>
             {numberOfItems} {numberOfItems > 1 ? 'productos' : 'producto'}{' '}
           </p>
         </div>
-        <div id='cart-details-subtotal'>
+        <div id='cart-details-subtotal' className={styles.cart_details_row}>
           <p>Subtotal</p>
           <p> {subTotal} € </p>
         </div>
-        <div id='cart-details-taxes'>
-          <p>Impuestos ( {TAX_RATE_PERCENTAGE} %)</p>
+        <div id='cart-details-taxes' className={styles.cart_details_row}>
+          <p>Impuestos ( {TAX_RATE_PERCENTAGE} % )</p>
           <p> {tax} € </p>
         </div>
-        <div id="cart-details-total">
-            <p>Total: </p>
-            <p> {total} € </p>
+        <div id='cart-details-total' className={styles.cart_details_row}>
+          <p>Total: </p>
+          <p> {total} € </p>
         </div>
       </div>
     )
--- src/pages/panel/index.tsx
+++ src/pages/panel/index.tsx
@@ -1,4 +1,5 @@
 import { CartDetails, CartProductList, ProductsList } from "@/features/shop";
+import { PublicLayout } from "@/layouts/public-layout";
 
 
 
@@ -7,11 +8,13 @@ export default function PanelPage( ) {
     const tag = undefined;
 
     return (
-      <div id='panel-page'>
-        <h1>PANEL PAGE</h1>
-        <ProductsList tag={tag} />
-        <CartProductList />
-        <CartDetails />
-      </div>
+      <PublicLayout>
+        <div id='panel-page'>
+          <h1>PANEL PAGE</h1>
+          <ProductsList tag={tag} />
+          <CartProductList />
+          <CartDetails />
+        </div>
+      </PublicLayout>
     )
 }
