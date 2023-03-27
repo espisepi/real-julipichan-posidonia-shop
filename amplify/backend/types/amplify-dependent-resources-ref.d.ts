8a6433d0e08fe51d5f76920d69f6c65d77d6985f
espisepi <sepinaco@gmail.com>
2023-03-27 13:52:07 +0200

    amplify pull --appId dla... --envName dev :added amplify storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3julipichanposidoniastoragebb47cc97": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
